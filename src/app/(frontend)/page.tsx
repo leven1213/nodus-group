eea59e610a7c198f6c7e648ebf9e4177aa955852
leven1213 <104328146@student.swin.edu.au>
2026-03-10 23:22:23 +1100

    force dynamic rendering on homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './page.module.css'
 
+export const dynamic = 'force-dynamic'
+
 export default async function HomePage() {
   const payload = await getPayload({ config: await config })
 
